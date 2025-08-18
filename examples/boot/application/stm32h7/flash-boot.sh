2098cb94b1a0e78c73da9990b84a44589b7b319f
Ekaterina <eo-savelyeva@xpanceo.com>
2025-08-18 19:53:20 +0400

    Boot not working, in progress

--- examples/boot/application/stm32h7/flash-boot.sh
+++ examples/boot/application/stm32h7/flash-boot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-probe-rs erase --chip STM32H743ZITx
+probe-rs erase --chip STM32H755ZITx
 mv ../../bootloader/stm32/memory.x ../../bootloader/stm32/memory-old.x
 cp memory-bl.x ../../bootloader/stm32/memory.x
 
-cargo flash --manifest-path ../../bootloader/stm32/Cargo.toml --release --features embassy-stm32/stm32h743zi --chip STM32H743ZITx --target thumbv7em-none-eabihf
+cargo +nightly flash --manifest-path ../../bootloader/stm32/Cargo.toml --release --features embassy-stm32/stm32h755zi-cm7 --chip STM32H755ZITx --target thumbv7em-none-eabihf
 
 rm ../../bootloader/stm32/memory.x
 mv ../../bootloader/stm32/memory-old.x ../../bootloader/stm32/memory.x
